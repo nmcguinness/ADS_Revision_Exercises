2ade4e6e88016daac3e4de95e4dc7543f16d9aa4
niall mcguinness <niall.mcguinness@dkit.ie>
2020-10-12 17:50:27 +0100

    Added selected solutions to pointers exercises 1 - 5

--- ADS_Revision_Exercises/ADS_Revision_Exercises.cpp
+++ ADS_Revision_Exercises/ADS_Revision_Exercises.cpp
@@ -14,8 +14,14 @@ void pointersExercise1();
 void operatorsExercise2();
 void operatorsExercise3();
 void operatorsExercise5();
+
 void pointersExercise1();
 void pointersExercise2();
+void pointersExercise3();
+void pointersExercise4();
+void pointersExercise5();
+void pointersExercise6();
+
 
 //other demos - array length, swap and adding comments to our code
 void usingArrayRefresher();
@@ -28,50 +34,24 @@ int main()
 {
     cout << "A&DS - Revision Exercises - Selected Solutions" << endl;
 
-    cout << endl << "Exercises: Operators..." << endl;
+    cout << endl << "Exercises: Classes & Operator Overloading..." << endl;
     operatorsExercise2();
     operatorsExercise3();
     operatorsExercise5();
 
-    cout << endl << "Exercises: Pointers..." << endl;
+    cout << endl << "Exercises: Pointers & Dynamic Memory..." << endl;
     pointersExercise1();
     pointersExercise2();
+    pointersExercise3();
+    pointersExercise4();
+    pointersExercise5();
+    pointersExercise6();
 
     cout << endl << "Other demos(array and swap)..." << endl;
     usingArrayRefresher();
     demoSimpleSwap();
 }
 
-
-void pointersExercise1() {
-
-    int* pArray = new int[3]; //12 bytes of space i.e. 3 x 4 bytes
-    pArray[0] = 15;  //*pArray = 15
-    pArray[1] = 25;  //*(pArray+1) = 25
-    pArray[2] = 35;  //*(pArray+2) = 35
-
-    int sum = getSumOfArray(pArray, 3);
-    cout << "Sum: " << sum << endl;
-
-    //always have a delete for every use of "new"
-    //note we use delete[] to delete a pointer to an array
-    delete[] pArray;
-}
-
-void pointersExercise2() {
-    int x = 10;
-    int y = 200;
-
-    int* pX = &x;
-    int* pY = &y;
-
-    swapPtrs(pX, pY);
-    cout << pX << endl; //addr
-    cout << *pX << endl;
-
-    cout << pY << endl; //addr
-    cout << *pY << endl;
-}
 
 /// @brief Demonstrates use of a getter for the Flower class
 void operatorsExercise2() {
@@ -92,6 +72,75 @@ void operatorsExercise5() {
     cout << f1 << endl;
 }
 
+/// @brief Demonstrates the getArrayTotal() function
+void pointersExercise1() {
+    int* pArray = new int[3]; //12 bytes of space i.e. 3 x 4 bytes
+    pArray[0] = 15;  //*pArray = 15
+    pArray[1] = 25;  //*(pArray+1) = 25
+    pArray[2] = 35;  //*(pArray+2) = 35
+
+    int sum = getArrayTotal(pArray, 3);
+    cout << "Sum: " << sum << endl;
+
+    //always have a delete for every use of "new"
+    //note we use delete[] to delete a pointer to an array
+    delete[] pArray;
+}
+
+/// @brief Demonstrates the swapPtrs() function
+void pointersExercise2() {
+    int x = 10;
+    int y = 200;
+
+    int* pX = &x;
+    int* pY = &y;
+
+    swapPtrs(pX, pY);
+    cout << pX << endl; //addr
+    cout << *pX << endl;
+
+    cout << pY << endl; //addr
+    cout << *pY << endl;
+}
+
+/// @brief Demonstrates the reverseArray() function
+void pointersExercise3()
+{
+    int ages[] = { 1, 2, 3, 4, 5, 6 };
+    int length = sizeof(ages) / sizeof(ages[0]);
+    reverseArray(ages, length);
+    printArray("ages", ages, length);
+}
+
+/// @brief Demonstrates the countEven() function
+void pointersExercise4()
+{
+    int nums[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    int length = sizeof(nums) / sizeof(nums[0]);
+    cout << "array countains " << countEven(nums, length) << " even values" << endl;
+}
+
+void pointersExercise5()
+{
+    //don't forget we can use scientific notation to represent small/large floating point values
+    double nums[] = { 6.8, 24.45, -45.67, 112.789, 8.67, 3.14, 1E-8};
+    int length = sizeof(nums) / sizeof(nums[0]);
+    double* pMax = getMaximum(nums, length);
+
+    if(pMax != nullptr)
+        cout << "max is " << *pMax << " at address[" << pMax << "]" << endl;
+    else
+        cout << "Array length was zero!" << endl;
+}
+
+void pointersExercise6()
+{
+    char str[] = { 'a', 'b', 'c', 'd', 'e', '\0'};
+
+    encrypt(str, 6, 1);
+}
+
+
 /********************************* CODE NOT RELATED TO EXERCISES *********************************/
 
 /// @brief Demonstrates how to use sizeof() to obtain the length of an array
@@ -99,7 +148,9 @@ void usingArrayRefresher() {
     //if we define an array using the standard method (i.e. not a pointer to an array e.g. int* pArray)
     //then we can determine the length of the array using sizeof()
     int ages[3];
-    ages[0] = 18; ages[1] = 22; ages[2] = 34;
+    ages[0] = 18; 
+    ages[1] = 22; 
+    ages[2] = 34;
 
     int length = sizeof(ages) / sizeof(ages[0]);
     cout << "Total size required for the array: " << sizeof(ages) << endl;
--- ADS_Revision_Exercises/functions.h
+++ ADS_Revision_Exercises/functions.h
@@ -7,15 +7,6 @@ int dummy(int x, float y) {
 	return -1;
 }
 
-/// @brief Swaps the addresses stored in two pointers to integers
-/// @param pA Reference to a pointer to a
-/// @param pB Reference to a pointer to b
-void swapPtrs(int* &pA, int* &pB) {
-	int* pTemp = pB;
-	pB = pA;
-	pA = pTemp;
-}
-
 /// @brief Demo function to show how to write a unit test
 /// @param x An integer x
 /// @param y An integer y
@@ -30,11 +21,13 @@ int divideAll(int x, int y, int z) {
 	return (x / y) / z;
 }
 
+/*************************************************************************/
+
 /// @brief Gets the sum of values in an array of integers
-/// @param pArray Pointer to the first element in the array
-/// @param length Length of the array
+/// @param pArray A pointer storing the address of the first element in the integer array
+/// @param length The length of the array
 /// @return Integer sum of all values in the array
-int getSumOfArray(int* pArray, int length) {
+int getArrayTotal(int* pArray, int length) {
 	int sum = 0;
 	for (int i = 0; i < length; i++) {
 		//sum += *(pArray + i); //we could write code like this too
@@ -43,4 +36,78 @@ int getSumOfArray(int* pArray, int length) {
 	return sum;
 }
 
+/// @brief Swaps the addresses stored in two pointers to integers
+/// @param pA Reference to a pointer to a
+/// @param pB Reference to a pointer to b
+void swapPtrs(int*& pA, int*& pB) {
+	int* pTemp = pB;
+	pB = pA;
+	pA = pTemp;
+}
+
+/// @brief Reverse the contents of an array using pointers
+/// @param pArray A pointer storing the address of the first element in the integer array
+/// @param length The length of the array
+void reverseArray(int* pArray, int length) {
+	//initialize the two pointers with the first and last address in the array
+	int* pLeft = pArray;
+	int* pRight = pArray + length - 1;
+	//variable to store the swapped value
+	int temp = 0;
+	while (pLeft < pRight) {
+		//dereference the number pointed to by pLeft and store in temp
+		temp = *pLeft;
+		//dereference the number at position pointed to by pRight and overwrite into position pointed to be pLeft
+		*pLeft = *pRight;
+		//write the stored value in temp into the position pointed to by pRight.
+		*pRight = temp;
+		//move the pointer to left towards the end of the array
+		pLeft++;
+		//move the pointer to right towards the start of the array
+		pRight--;
+	}
+}
+
+/// @brief Returns a count of the number of even integers in the array
+/// @param pArray A pointer storing the address of the first element in the integer array
+/// @param length The length of the array
+/// @return 
+int countEven(int* pArray, int length) {
+	int count = 0;
+
+	for (int i = 0; i < length; i++)
+		if (pArray[i] % 2 == 0) count++;
+
+	return count;
+}
+
+/// @brief Returns a count of the number of even integers in the array
+/// @param pArray A pointer storing the address of the first element in the double array
+/// @param length The length of the array
+/// @return Pointer to the maximum value in the array 
+double* getMaximum(double* pArray, int length) {
+
+	if (length == 0)
+		return nullptr;
+
+	double max = -DBL_MAX; //the negative of the largest value
+
+	for (int i = 0; i < length; i++) {
+		if (pArray[i] > max) {
+			max = pArray[i];
+		}
+	}
+
+	//return a pointer (i.e. the address of max)
+	return &max;
+}
+
+/// @brief Utility function to print an array of integer values
+/// @param id A string identifier useful when printing out the array (e.g. "Student Ages[0]...")
+/// @param pArray A pointer storing the address of the first element in the integer array
+/// @param length The length of the array
+void printArray(string id, int* pArray, int length) {
+	for (int i = 0; i < length; i++)
+		cout << id << "[" << i << "]: " << pArray[i] << endl;
+}
 
